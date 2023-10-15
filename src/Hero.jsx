2d5a68514872a1e695b6f740f4282f9c910e0f8b
Lim Shi Jie <shijielim@gmail.com>
2023-10-15 22:21:52 +0800

    finished

---
+++ src/Hero.jsx
@@ -0,0 +1,23 @@
+import heroImg from "./assets/hero.svg";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque
+            quaerat corporis temporibus porro animi repellendus, reiciendis,
+            fuga corrupti atque sequi illo excepturi blanditiis provident alias,
+            nostrum aliquid incidunt eaque aperiam!
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
---
+++ src/Projects.jsx
@@ -0,0 +1,37 @@
+import { useFetchProjects } from "./fetchProject";
+
+const Projects = () => {
+  const { loading, projects } = useFetchProjects();
+  if (loading) {
+    return (
+      <section className="projects">
+        <h2>Loading ...</h2>
+      </section>
+    );
+  }
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        {projects.map(({ title, url, id, img }) => {
+          return (
+            <a
+              key={id}
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              className="project"
+            >
+              <img src={img} alt={title} className="img" />
+              <h5>{title}</h5>
+            </a>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+export default Projects;
